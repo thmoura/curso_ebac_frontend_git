05cfa2ad89cc5d53ca8a798b6b934db93b70ee70
Thiago Moura <thiagoreismoura@gmail.com>
2023-02-15 20:25:29 -0300

    Ajustes

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const form = document.getElementById('form-maior');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const campoA  = parseInt(document.getElementById("campo-a").value,10);
-    const campoB = parseInt(document.getElementById("campo-b").value,10);
+    var campoA  = parseInt(document.getElementById("campo-a").value,10);
+    var campoB = parseInt(document.getElementById("campo-b").value,10);
 
     if (campoA  < campoB ) {
         alert("Formulario valido");
