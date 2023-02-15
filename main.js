8ecbaa59b209c37a6767b4b353b5a0b33c727107
Thiago Moura <thiagoreismoura@gmail.com>
2023-02-15 20:06:12 -0300

    Ajustes

--- main.js
+++ main.js
@@ -3,17 +3,13 @@ const form = document.getElementById('form-maior');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const campoA  = document.getElementById('campo-a');
-    const campoB = document.getElementById('campo-b');
+    const campoA  = parseInt(document.getElementById("campo-a").value,10);
+    const campoB = parseInt(document.getElementById("campo-b").value,10);
 
-    if (campoA.value  < campoB.value ) {
+    if (campoA  < campoB ) {
         alert("Formulario valido");
-        campoA.value  = '';
-        campoB.value = '';
     } else {
         alert("Formulario invalido");
-        campoA.value  = '';
-        campoB.value = '';
     }
 
 })
