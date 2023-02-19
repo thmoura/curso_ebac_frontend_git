897fe6d171a7b561bda1968ea2be05e80f9bb340
Thiago Moura <thiagoreismoura@gmail.com>
2023-02-19 13:57:57 -0300

    Ajustes

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 const form = document.getElementById('form-lista')
+const tarefa = [];
+
+let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    
+    adicionarlinha();
+    atualizaTabela();
 })
+
+function adicionarlinha(){
+    const inputNovaTarefa = document.getElementById('nome-tarefa');
+
+    if(tarefa.includes(inputNovaTarefa.value)){
+        alert(`A tarefa: ${inputNovaTarefa} já foi cadastrada.`)
+    }
+    else {
+        tarefa.push(inputNovaTarefa.value);
+
+        let linha = `<tr>`;
+        linha += `<td>${inputNovaTarefa.value}</td>`;
+
+        linhas += linha;
+    }
+
+    inputNovaTarefa.value = '';
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas
+
+}
