6a491ada6151b845eccc9d93c92767e7d6e5bed1
thiago <thiagoreismoura@gmail.com>
2023-05-13 17:30:01 -0300

    Ajustes

--- main.js
+++ main.js
@@ -1,42 +1,21 @@
 $(document).ready(function (){
 
-    const form = document.getElementById('form-lista')
-    const tarefa = [];
-
-    let linhas = '';
 
     $('form').on('submit', function(e){
         e.preventDefault();
 
-        adicionarlinha();
-        atualizaTabela();
+        const inputNovaTarefa = $('#nome-tarefa').val();
+        const novoItem = $('<li></li>');
+        
+        // $('li').addClass('ponto');
+        $(`<li>${inputNovaTarefa}</li>`).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $('li').click(function(){
+            $(this).addClass('feito');
+        });
+        $('#nome-tarefa').val(" ");
+    
+
     });
-
-    function adicionarlinha(){
-        const inputNovaTarefa = document.getElementById('nome-tarefa');
-
-        if(tarefa.includes(inputNovaTarefa.value)){
-            alert(`A tarefa: ${inputNovaTarefa} já foi cadastrada.`)
-        }
-        else {
-            tarefa.push(inputNovaTarefa.value);
-
-            let linha = `<tr>`;
-            linha += `<li>${inputNovaTarefa.value}</li>`;
-
-            linhas += linha;
-        }
-
-        inputNovaTarefa.value = '';
-    }
-
-    function atualizaTabela(){
-        const corpoTabela = document.querySelector('tbody');
-        corpoTabela.innerHTML = linhas
-    }
-
-    $('li').click(function(){
-        linhas =  "text-decoration: line-through";
-    })
 
 })
